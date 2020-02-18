ea84f967b77e7d4ac041708b6caab904a516c129
Nicholas Cox <nick.a.cox@gmail.com>
2020-02-18 16:21:40 -0600

    Updates

---
+++ precache-manifest.b810b2f848d97989851e2aa39801584a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f28b718d708f7f704cdf4d136e07747",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "001edc85f60b9a69c745",
+    "url": "/drum-machine/static/css/main.17eedb03.chunk.css"
+  },
+  {
+    "revision": "ff6cb366869f8081dca6",
+    "url": "/drum-machine/static/js/2.44269fdb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/drum-machine/static/js/2.44269fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001edc85f60b9a69c745",
+    "url": "/drum-machine/static/js/main.7a219677.chunk.js"
+  },
+  {
+    "revision": "507a9231a935b1c59061",
+    "url": "/drum-machine/static/js/runtime-main.e050a463.js"
+  },
+  {
+    "revision": "e35bff252e2938f174fe2974babb0928",
+    "url": "/drum-machine/static/media/zzwool.e35bff25.png"
+  }
+]);
