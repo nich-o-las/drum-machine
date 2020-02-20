10f3a8806fcd273bcd6e20ab243a69936066c57f
Nicholas Cox <nick.a.cox@gmail.com>
2020-02-20 17:50:27 -0600

    style updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import DrumMachine from './Components/DrumMachine/DrumMachine';
+import Title from './Components/Title/Title';
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <DrumMachine />
     </div>
   );
